cb9adc5f73113de24c49ab0d40302e20d8223cf3
Guy Kongthong <guykongthongcmu@gmail.com>
2025-02-10 20:01:52 +0700

    Update coinMain.cpp

--- coinMain.cpp
+++ coinMain.cpp
@@ -1,30 +1,22 @@
-// Coding Solution for Coin Change Problem
+// solution for coin change problem
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class coin {
-    public:
-        int n;
 
-        coin(int n) {
-            this->n = n;
+int sol(vector<int> coins, int n) {
+    vector<int> combo(n + 1, 0);
+    combo[0] = 1;
+
+    for (int i = 0; i < coins.size(); i++) {
+        for (int j = coins[i]; j <= n; j++) {
+            combo[j] += combo[j - coins[i]];
         }
-
-        int sol(vector<int> coins) {
-            vector<int> combo(n + 1, 0);
-            combo[0] = 1;
-
-            for (int i = 0; i < coins.size(); i++) {
-                for (int j = coins[i]; j <= n; j++) {
-                    combo[j] += combo[j - coins[i]];
-                }
-            }
-            return combo[n];
-        }
-};
+    }
+    return combo[n];
+}
 
 int coinMain() {
     vector<int> testArr;
@@ -32,8 +24,7 @@ int coinMain() {
     testArr.push_back(5);
     testArr.push_back(3);
     testArr.push_back(6);
-    coin test(10);
-    cout << test.sol(testArr) << endl;
+    cout << sol(testArr, 10) << endl;
     return 0;
 }
 
